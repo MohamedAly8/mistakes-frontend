e82f07912f0ee90838627714ed18cae7fd4d483f
Mohamed Aly <mohamed_alyaly@outlook.com>
2025-04-18 23:07:41 -0400

    Initialized frontend

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,9 @@
+const NavigationBar = () => {
+  return (
+    <div className="flex items-center justify-between p-4 bg-gray-800">
+      <div className="text-lg font-bold">Mistakes</div>
+    </div>
+  );
+};
+
+export default NavigationBar;
