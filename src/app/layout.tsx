c95eb9ce14ff4ff8f1b92ceb778c4dc35899c749
Mohamed Aly <mohamed_alyaly@outlook.com>
2025-04-21 23:48:43 -0400

    Removed unused functions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-neue-haas",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-neue-haas",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Mistakes",
